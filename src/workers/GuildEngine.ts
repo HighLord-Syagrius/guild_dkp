356b229332e0a30b144b687355a0858b54c0894f
Highlord-Syagrius <brendan.e.black@gmail.com>
2021-08-26 00:22:43 -0600

    added stacktrace for main engine

--- src/workers/GuildEngine.ts
+++ src/workers/GuildEngine.ts
@@ -8,19 +8,23 @@ import { Guild, Player } from '../typeSafety/definitions';
  * @param params `object` anything needed to satiate the params for the action
  */
 export default function (action: (p: Player, params?: object) => Player, players?: string[], params?: object): void {
-	stat("./players.json", (err: NodeJS.ErrnoException) => {
-		if (err) { throw new Error().stack; }
-		readFile("../history/players.json", (readErr: NodeJS.ErrnoException, data: Buffer) => {
-			const guild = <Guild>JSON.parse(data.toString());
-			if (readErr) { throw new Error().stack; }
+	try {
+		stat("./players.json", (statErr: NodeJS.ErrnoException) => {
+			if (statErr) { throw new Error().stack; }
+			readFile("../history/players.json", (readErr: NodeJS.ErrnoException, data: Buffer) => {
+				const guild = <Guild>JSON.parse(data.toString());
+				if (readErr) { throw new Error().stack; }
 
-			// this sequence is actually pretty funny
-			(players ? players : Object.keys(guild)).forEach(p => {
-				typeof params == "string" && (<string>params).toUpperCase() == "FUCKFELDMAN" ?  // TODO, log historical members? or just leave to git history?
-					(delete guild[p])
-					: (guild[p] = action(guild[p], params));
-			});
-			writeFile("../history/players.json", JSON.stringify(guild), () => console.log("SHOTS FOR SYA"))
+				// this sequence is actually pretty funny
+				(players ? players : Object.keys(guild)).forEach(p => {
+					typeof params == "string" && (<string>params).toUpperCase() == "FUCKFELDMAN" ?  // TODO, log historical members? or just leave to git history?
+						(delete guild[p])
+						: (guild[p] = action(guild[p], params));
+				});
+				writeFile("../history/players.json", JSON.stringify(guild), () => console.log("SHOTS FOR SYA"))
+			})
 		})
-	})
+	} catch (e) {
+		console.log(`[${new Date}]: ${new Error().stack}`)
+	}
 }

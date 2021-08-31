6ab87fd669c98e89656da5ed668e471e6a5fdbe9
Highlord-Syagrius <brendan.e.black@gmail.com>
2021-08-31 00:22:32 -0600

    cleared gitignore

---
+++ src/private/endeavors.ts
@@ -0,0 +1,7 @@
+//@ts-expect-error
+import Actions from './workers/actions';
+//@ts-expect-error
+import GuildEngine from './workers/GuildEngine';
+//@ts-expect-error
+import { Player } from './private/parts';
+
